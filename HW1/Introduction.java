dfeaf1b866e9975b178b872ec433c6305d41e642
HoangLinh2k6 <do16092006@gmail.com>
2025-09-20 21:16:43 +0700

    Create Introduction.java

---
+++ HW1/Introduction.java
@@ -0,0 +1,12 @@
+public class Introduction {
+    public static void main( String[] args )
+    {
+        System.out.println( "HoangTrongNhatLinh\t24021546\tCS5\tHoangLinh2k6\tdo16092006@gmail.com");
+        for( int i = 9; i >= 1; i-- )
+        {
+            System.out.println( i + " bottles of beer on the wall, " + i + " bottles of beer." );
+            System.out.println( "Take one down, pass it around," );
+        }
+        System.out.println( "No more bottles of beer on the wall." );
+    }
+}
